8c9b6dcdc00e7c81251e4aa2e9105f6a60ebd713
AaronRobert <Robert.Aaron2017@outlook.com>
2019-01-26 00:25:13 +0800

    角色移动-初步

--- Home/Assets/Scripts/Player.cs
+++ Home/Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    Vector2 moveDest;
+    public float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        moveDest = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            moveDest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        var t = Time.deltaTime * speed;
+        if(Mathf.Abs(moveDest.x - transform.position.x) >= t)
+        {
+            transform.position = new Vector2(transform.position.x + (moveDest.x > transform.position.x ? t : -t), transform.position.y);
+        }
     }
 }
