596a1a9d4759fa87431a341c3ab417e6a5411741
AaronRobert <Robert.Aaron2017@outlook.com>
2019-01-26 16:28:34 +0800

    完成气泡机制

--- Home/Assets/Scripts/Entity.cs
+++ Home/Assets/Scripts/Entity.cs
@@ -8,19 +8,26 @@ public class Entity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ShowBuble("robert");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ShowBuble("robert"));
+        }
     }
 
-    public void ShowBuble(string bubleName)
+    IEnumerator ShowBuble(string bubleName)
     {
+        m_buble.SetActive(true);
         GameObject go = Manager.instance.GetBubleContent(bubleName);
         go.transform.parent = m_buble.transform;
         go.transform.localPosition = Vector3.zero;
+        yield return new WaitForSeconds(1f);
+        Destroy(go);
+        m_buble.SetActive(false);
     }
 }
