e2d86068c06cda2ca948cdfe24a471f8571a74af
defoliation <defoli_ation@outlook.com>
2019-01-26 16:08:30 +0800

    add animations

---
+++ Home/Assets/Scripts/Path/PathAI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathAI : MonoBehaviour
+{
+
+    public List<Path> paths;
+    public GameObject gameObject;
+    
+    public void startGo()
+    {
+        
+    }
+
+
+}
