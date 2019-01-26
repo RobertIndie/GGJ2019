c4dc7f18920fb6c580f41492402cb28139e11b0d
defoliation <defoli_ation@outlook.com>
2019-01-26 21:51:20 +0800

    add animation to player

--- Home/Assets/Scripts/Player.cs
+++ Home/Assets/Scripts/Player.cs
@@ -1,38 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
-    Rigidbody2D rigidbody2D;
-    Vector2 moveDest;
-    public float speed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        moveDest = transform.position;
-        GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        base.Update();
         if (Input.GetMouseButtonDown(0))
         {
             moveDest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
-        var t = Time.deltaTime * speed;
-        if (Mathf.Abs(moveDest.x - transform.position.x) >= t)
-        {
-            //transform.position = new Vector2(transform.position.x + (moveDest.x > transform.position.x ? t : -t), transform.position.y);
-            rigidbody2D.MovePosition(new Vector2(transform.position.x + (moveDest.x > transform.position.x ? t : -t), transform.position.y));
-        }
     }
 
-    public void Stop()
-    {
-        moveDest = transform.position;
-    }
 }
