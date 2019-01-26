b1d34943e00c24027c73f0af2d2010f66fda301b
defoliation <defoli_ation@outlook.com>
2019-01-27 01:02:53 +0800

    update

---
+++ Home/Assets/Scripts/EntityFilpAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityFilpAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    public Entity entity;
+
+    void Update()
+    {
+        animator.SetBool("stop",entity.stop);
+        animator.SetBool("leftwalk",entity.leftWalk);
+        animator.SetBool("rightwalk",entity.rightWalk);
+    }
+}
