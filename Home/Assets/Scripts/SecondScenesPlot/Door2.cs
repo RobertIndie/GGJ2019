d7b0b87bd1224a677670d72f9c2cdf3faa34bea5
defoliation <defoli_ation@outlook.com>
2019-01-27 11:16:19 +0800

    update

--- Home/Assets/Scripts/SecondScenesPlot/Door2.cs
+++ Home/Assets/Scripts/SecondScenesPlot/Door2.cs
@@ -1,12 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class Door2 : Door
 {
+
+    public Entity son;
+
+    public Player pieter;
+    
+    
     // Start is called before the first frame update
     public override void Open()
     {
+        StartCoroutine(start());
+    }
+
+    IEnumerator start()
+    {
+        pieter.isControllable = false;
+        goTo(pieter,-6.65f);
+        goTo(son,-4.39f);
         
+    }
+
+
+
+    void goTo(Entity s, float to)
+    {
+        s.moveDest = getMove(s.moveDest, to);
+    }
+    
+    Vector2 getMove(Vector2 v, float to)
+    {
+        return new Vector2(to,v.y);
     }
 }
